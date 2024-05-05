e1288d487ce3251c9999d7bb985a97158ab2b84e
Marvin Ronquillo <mondejarmarron18@gmail.com>
2024-05-05 10:57:02 +0800

    refactor:loading component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,12 @@ import Typewriter from "typewriter-effect";
 export default function Home() {
   return (
     <main className="min-h-screen grid place-items-center">
-      <div className="flex relative flex-col size-[200px] lg:size-[250px] animate-pulse items-center justify-center gap-4">
+      <div className="flex relative flex-col size-[200px] animate-pulse items-center justify-center gap-4">
         <div className="h-full w-full absolute border-gray-600 drop-shadow-lg border-t-2 rounded-full animate-spin duration-[3000ms]"></div>
         <span className="scale-x-[-1]">
-          <FaHammer className="duration-1000 origin-bottom-left animate-hammering size-[40px] lg:size-[60px]" />
+          <FaHammer className="duration-1000 origin-left animate-hammering size-[40px]" />
         </span>
-        <span className="text-md lg:text-xl font-medium">
+        <span className="text-md font-semibold uppercase">
           <Typewriter
             options={{
               strings: ["iForgeTech", "Forging Soon"],
