88c6a830991f0922a0106e1a1b5eed07fe8e714d
Marvin Ronquillo <mondejarmarron18@gmail.com>
2024-05-05 11:02:47 +0800

    feat:global opengraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,20 @@ const inter = Montserrat({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "iForgeTech",
   description: "Forging Technologies",
+  openGraph: {
+    title: "iForgeTech",
+    description: "Forging Technologies",
+    url: "https://iforgetech.com",
+    siteName: "iForgeTech",
+    images: [
+      {
+        url: "https://images.unsplash.com/photo-1460925895917-afdab827c52f?q=80&w=1715&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        width: 1920,
+        height: 1080,
+      },
+    ],
+    type: "website",
+  },
 };
 
 export default function RootLayout({
