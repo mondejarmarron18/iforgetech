d38a1678468db00b0adcc4246fab06cc9e7ff28d
Marvin Ronquillo <mondejarmarron18@gmail.com>
2024-05-06 18:50:09 +0800

    fix:body height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Typewriter from "typewriter-effect";
 
 export default function Home() {
   return (
-    <main className="min-h-screen grid place-items-center">
+    <main className="h-full grid place-items-center">
       <div className="flex relative flex-col size-[200px] animate-pulse items-center justify-center gap-4">
         <div className="h-full w-full absolute border-gray-600 drop-shadow-lg border-t-2 rounded-full animate-spin duration-[3000ms]"></div>
         <span className="scale-x-[-1]">
