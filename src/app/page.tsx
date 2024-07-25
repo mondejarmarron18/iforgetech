89e99d64ce026a6703268fa0b4d588cb011483c4
Marvin Ronquillo <mondejarmarron18@gmail.com>
2024-07-25 23:26:32 +0800

    feat:portfolio preview btn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import Typewriter from "typewriter-effect";
 import { useTranslation } from "react-i18next";
 import Footer from "@/components/Footer";
 import "@/libs/i18n";
+import Translate from "@/components/Translate";
 
 export default function Home() {
   const { t } = useTranslation();
@@ -19,22 +20,32 @@ export default function Home() {
         </div>
         <LanguageDropDown />
       </div>
-      <div className="flex relative flex-col size-[200px] animate-pulse items-center justify-center gap-4">
-        <div className="h-full w-full absolute border-gray-800 drop-shadow-xl border-t-2 rounded-full animate-spin"></div>
-        <span className="scale-x-[-1]">
-          <FaHammer className="duration-1000 origin-left animate-hammering size-[40px]" />
-        </span>
-        <span className="text-md font-semibold text-center">
-          <Typewriter
-            options={{
-              strings: [t("comingSoon")],
-              autoStart: true,
-              loop: true,
-              delay: "natural",
-              deleteSpeed: "natural",
-            }}
-          />
-        </span>
+      <div className="flex flex-col gap-5 justify-center items-center">
+        <div className="flex relative flex-col size-[200px] animate-pulse items-center justify-center gap-4">
+          <div className="h-full w-full absolute border-gray-800 drop-shadow-xl border-t-2 rounded-full animate-spin"></div>
+          <span className="scale-x-[-1]">
+            <FaHammer className="duration-1000 origin-left animate-hammering size-[40px]" />
+          </span>
+          <span className="text-md font-semibold text-center">
+            <Typewriter
+              options={{
+                strings: [t("comingSoon")],
+                autoStart: true,
+                loop: true,
+                delay: "natural",
+                deleteSpeed: "natural",
+              }}
+            />
+          </span>
+        </div>
+        <div className="text-center px-4">
+          <p className="text-lg mb-5">{t("underConstruction")}</p>
+          <a href="https://portfolio.iforgetech.com">
+            <button className="font-medium text-white bg-slate-700 hover:bg-slate-600 rounded px-6 py-4">
+              {t("visitPreview")}
+            </button>
+          </a>
+        </div>
       </div>
       <Footer />
     </main>
--- src/libs/i18n/index.ts
+++ src/libs/i18n/index.ts
@@ -16,18 +16,24 @@ i18n.init({
       translation: {
         comingSoon: "Forging Soon",
         contactMe: "Contact me at",
+        underConstruction: "The site is currently under construction.",
+        visitPreview: "Visit Portfolio Preview",
       },
     },
     ja: {
       translation: {
         comingSoon: "すぐに鍛造",
         contactMe: "メールアドレス",
+        underConstruction: "サイトは現在建設中です。",
+        visitPreview: "ポートフォリオのプレビューにアクセス",
       },
     },
     fi: {
       translation: {
         comingSoon: "Malapit na",
         contactMe: "Makipag-ugnayan sa akin sa",
+        underConstruction: "Ang site ay kasalukuyang ginagawa.",
+        visitPreview: "Bisitahin ang pasilip sa portpolyo",
       },
     },
   },
